af66b4c1ce900e67bad840907739779d54fb05b7
Rutucode <rutujapawarhere08@gmail.com>
2024-03-23 23:12:58 +0530

    division

---
+++ division.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{ 
+    float dividend,divisor, ans;
+
+    printf("Enter value of dividend \n");
+
+    scanf("%f", &dividend);
+
+    printf( "Enter value of divisor \n");
+
+    scanf("%f", &divisor);
+
+    ans= dividend / divisor;
+
+    printf("\n %f / %f = %f", dividend, divisor, ans );
+
+    return 0;
+
+
+
+
+
+
+
+}
