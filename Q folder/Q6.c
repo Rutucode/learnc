c9c9ae930bb6d6fa17c57dc8972c27a6aa516c52
Rutucode <rutujapawarhere08@gmail.com>
2024-04-13 22:54:12 +0530

    Q folder/Q6.c         Q folder/Q7.c

---
+++ Q folder/Q6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+int number;
+int number;
+printf("Enter a number\n");
+scanf("%d",&number);
+
+if(number % 2 == 0 &&number %5 == 0){
+    printf("%d is even number and divisible by 5");
+}
+else if (number % 2 == 0  && number % 5!= 0 )
+{
+    printf("%d is even number and not ")
+}
+
+    return 0;
+}
+
+/*Write a program to find whether given number is even and divisible by 5 or not.*/
---
+++ Q folder/Q7.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    
+}
+/*Write a program accept marks of 5 subjects calculate the percentage and display grade of student from percentage( grade : Distinction, first class, second class, pass, fail)*/
